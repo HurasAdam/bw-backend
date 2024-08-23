f4c581537539508d0a5bd59055678e09286b47a1
HurasAdam <huradam@wp.pl>
2024-08-24 00:45:46 +0200

    Commit inicializujacy strone backendowa, dodano schemat modelu Uzytkownika.

---
+++ src/models/User.ts
@@ -0,0 +1,12 @@
+import {Schema,model} from "mongoose";
+
+const userSchema = new Schema({
+name:{type:String, required:true},
+surname:{type:String, required:true},
+email:{type:String, required:true},
+isAdming:{type:Boolean, default:false},
+isVerified:{type:Boolean, default:false},
+
+},{timestamps:true});
+
+const User = model("User",userSchema);
