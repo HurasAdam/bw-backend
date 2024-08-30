231b81c807870ac0509467b990d6b38df70fae85
HurasAdam <huradam@wp.pl>
2024-08-30 23:53:14 +0200

    Dodano: Obsługę dodwania oraz pobierania listy artykułow.

--- src/models/Article.ts
+++ src/models/Article.ts
@@ -2,14 +2,14 @@ import {model,Schema} from "mongoose";
 
 const articleSchema = new Schema({
     title:{type:String, required:true},
-    employeeDescription:{type:String},
+    employeeDescription:{type:String,required:true},
     createdBy:{type:Schema.Types.ObjectId, ref:"User", required:true},
-    tags:{type:Schema.Types.ObjectId, ref:"Tag", required:true,},
+    tags:[{type:Schema.Types.ObjectId, ref:"Tag", required:true,}],
     clientResponse:{type:String, required:true},
-verified:{
+
     isVerified:{type:Boolean, required:true, default:false},
     verifiedBy:{type:Schema.Types.ObjectId, ref:"User", required:true,}
-}
+
 },{timestamps:true});
 
 
