6dec9e2bd82469c9b88c5f27c991fec77e1eb311
HurasAdam <huradam@wp.pl>
2024-08-31 22:19:57 +0200

    Dodano: Obsługę paginacji dla listy pobieranych artykułów.

--- src/controllers/articleControllers.ts
+++ src/controllers/articleControllers.ts
@@ -1,43 +1,81 @@
-import { NextFunction, Request,Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import Article from "../models/Article";
- const createArticle= async(req:Request,res:Response,next:NextFunction)=>{
-    try{
-        const { title, clientResponse, employeeDescription, verifiedBy, tags, createdBy }=req.body;
+const createArticle = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { title, clientDescription, employeeDescription, verifiedBy, tags } =
+      req.body;
+    const user = req.user;
+    console.log(user);
+    const article = await Article.findOne({ title });
 
-const article = await Article.findOne({title});
-
-console.log(article)
-
-if(article){
-    return res.status(400).json({message:"Article with that title already exists"})
-}
-const createdTag = await Article.create({
-    title,clientResponse,employeeDescription,verifiedBy,tags,createdBy
-})
-res.status(200).json(createdTag);
-    }catch(error){
-        console.log(error);
-        next(error);
+    if (article) {
+      return res
+        .status(400)
+        .json({ message: "Article with that title already exists" });
     }
+    const createdTag = await Article.create({
+      title,
+      clientDescription,
+      employeeDescription,
+      verifiedBy: user?.userId,
+      tags,
+      createdBy: user?.userId,
+    });
+    res.status(200).json(createdTag);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 };
 
+const getAllArticles = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const pageSize = 15;
+    const pageNumber = parseInt(
+      req.query.page ? req.query.page.toString() : "1"
+    );
+    const skipp = (pageNumber - 1) * pageSize;
 
-const getAllArticles= async(req:Request,res:Response,next:NextFunction)=>{
-    try{
-const tags= await Article.find({});
+    const articles = await Article.find({})
+      .select([
+        "-clientDescription",
+        "-employeeDescription",
+        "-createdBy",
+        "-verifiedBy",
+        "-createdAt",
+        "-__v",
+      ])
+      .populate([{ path: "tags", select: ["name"] }])
+      .skip(skipp)
+      .limit(pageSize);
 
+    const total = await Article.countDocuments();
 
-res.status(200).json(tags);
-    }catch(error){
-        console.log(error);
-        next(error);
-    }
-} 
+    const responseObject = {
+      data: articles,
+      pagination: {
+        total,
+        page: pageNumber,
+        pages: Math.ceil(total / pageSize),
+      },
+    };
 
+    res.status(200).json(responseObject);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
 
-
-
-export const articleController ={
-    createArticle,
-    getAllArticles
-}
+export const articleController = {
+  createArticle,
+  getAllArticles,
+};
